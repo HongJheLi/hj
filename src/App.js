f83465cb3e71a74590792b744a943612fe8b3a7e
HongJhe <hongjhe@kfsyscc.org>
2018-03-07 10:37:20 +0800

    change.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src='http://www.president.gov.tw/images/president/flag.jpg' alt="logo" />
+          <h1 className="App-title">I Love my country</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
